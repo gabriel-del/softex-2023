385ad5dbe099d4768ee955c0f0f35e73084b5368
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-08-10 01:45:21 -0300

    m2e8

--- plataforma/1-logica/m2/8.js
+++ plataforma/1-logica/m2/8.js
@@ -3,11 +3,7 @@ let candidatos = [
     {nome: "candidato_Y", id: 847, votos: 0},
     {nome: "candidato_Z", id: 515, votos: 0},
     {nome: "branco", id: 0, votos: 0},
-    {nome: "nulo", id: "Outro", votos: 0}
-]
-
-const listarCandidatos = () => {candidatos.forEach(candidato => console.log(`${candidato.nome}: ${candidato.id}`))}
-const listarCandidatosFinal = () => {candidatos.forEach(candidato => console.log(`${candidato.nome}: ${candidato.votos}`))}
+    {nome: "nulo", id: "Outro", votos: 0}]
 
 const pedirVoto = async () => {
   const rl = require('readline').createInterface({input: process.stdin, output: process.stdout});
@@ -20,27 +16,30 @@ const finalizarVotacao = async () => {
   const rl = require('readline').createInterface({input: process.stdin, output: process.stdout});
   const  finalizar = await new Promise(finalizar => rl.question("Deseja finalizar a votação? (s/n) ", finalizar))
   rl.close()
-  return finalizar === "s"
-}
+  return finalizar === "s"}
+
+const listarCandidatos = () => {candidatos.forEach(({nome,id}) => console.log(`\t${nome}: ${id}`))}
+const listarCandidatosFinal = () => {candidatos.forEach(({nome,votos}) => console.log(`\t${nome}: ${votos} votos`))}
+
+const listarVencedor = () => {
+ const max = candidatos.reduce( (acc, current) => current.votos > acc ? current.votos : acc , 0 )
+ candidatos.filter(({votos}) => votos === max).forEach(({nome, votos}) => console.log(`\t${nome}: ${votos} votos`))}
   
 const pegarVotos = async () => {
   let candidatosId=[]
   candidatos.forEach(candidato => candidatosId.push(candidato.id))
   while(true){
-  voto = await pedirVoto()
-  if (candidatosId.includes(voto)) { candidatos.filter(candidato => candidato.id === voto)[0].votos++}
-  else if (isNaN(voto)) { console.log("Inválido: vote novamente")}
-  else { candidatos.filter(candidato => candidato.nome === "nulo")[0].votos++ }
-  if (await finalizarVotacao()) break
-  }
- }
+    voto = await pedirVoto()
+    if (candidatosId.includes(voto)) { candidatos.filter(candidato => candidato.id === voto)[0].votos++}
+    else if (isNaN(voto)) { console.log("Inválido: vote novamente") ;continue}
+    else { candidatos.filter(candidato => candidato.nome === "nulo")[0].votos++ }
+    if (await finalizarVotacao()) break }}
 
 (async () => {
-
-await pegarVotos()
-// console.log(candidatos)
-listarCandidatosFinal()
+  await pegarVotos()
+  console.log("\nResultado Final: ")
+  listarCandidatosFinal()
+  console.log("\nVencedor(es): ")
+  listarVencedor()
 })()
 
-// TODO: string: repetir
-// TODO: mostrar vencedor
