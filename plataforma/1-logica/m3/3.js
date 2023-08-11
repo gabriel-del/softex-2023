242396ef0074f3d83cc2de17d4347554fe547d5b
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-08-11 03:54:45 -0300

    m3e3

---
+++ plataforma/1-logica/m3/3.js
@@ -0,0 +1,14 @@
+let array = [3, 7, 9, 1, 0]
+
+//fila
+console.log(array.shift())
+//: Do primeiro ao último
+
+//lista
+console.log(array.pop())
+//: Do último ao primeiro
+
+//pilha
+array.splice(0, 1)
+console.log(array)
+//: Do primeiro ao último
